1046d1ce4daa8ec12a9f65bcbe5c99652818bd5d
FurkanGenc <ffurkangenc@gmail.com>
2026-03-19 13:19:12 +0300

    Add project files.

---
+++ OtelOtomasyon.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OtelOtomasyon.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OtelOtomasyon.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OtelOtomasyon.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
